783deb55be5eaf0e018423d591760441253aa0bf
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2016-01-09 03:47:21 +0100

    Fixed some naming bugs

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface {
 	public function getConfigTreeBuilder() {
 		$treeBuilder = new TreeBuilder();
 		//$rootNode    = $treeBuilder->root('ejt_file');
-		$treeBuilder->root('ejt_file');
+		$treeBuilder->root('syren7_owncloud');
 
 		// Here you should define the parameters that are allowed to
 		// configure your bundle. See the documentation linked above for
