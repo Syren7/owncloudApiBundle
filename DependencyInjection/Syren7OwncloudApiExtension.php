a49a8a7c72d155b6da579433ca0069cc7a064bca
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2016-01-09 18:17:41 +0100

    Naming fix

--- DependencyInjection/Syren7OwncloudApiExtension.php
+++ DependencyInjection/Syren7OwncloudApiExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class syren7OwncloudApiExtension extends Extension {
+class Syren7OwncloudApiExtension extends Extension {
 	/**
 	 * {@inheritdoc}
 	 */
