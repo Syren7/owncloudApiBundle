f8c2d8bca1aabfe80d1ab38f8016628598a8dded
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2016-01-09 18:15:27 +0100

    Fixed loading of bundle extension

--- Syren7OwncloudApiBundle.php
+++ Syren7OwncloudApiBundle.php
@@ -3,7 +3,10 @@
 namespace Syren7\OwncloudApiBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Syren7\OwncloudApiBundle\DependencyInjection\Syren7OwncloudApiExtension;
 
-class Syren7OwncloudApiBundle extends Bundle
-{
+class Syren7OwncloudApiBundle extends Bundle {
+	public function getContainerExtension() {
+		return new Syren7OwncloudApiExtension();
+	}
 }
