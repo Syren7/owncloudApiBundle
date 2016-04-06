7139374da5e32e1baf70b92b61da47cc5d37f31f
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2016-04-06 12:45:54 +0200

    set owncloud folder as optional parameter. If not set its '' per default

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -32,7 +32,7 @@ class Configuration implements ConfigurationInterface {
 					->cannotBeEmpty()
 				->end()
 				->scalarNode('folder')
-					->isRequired()
+					->defaulValue('')
 				->end()
 			->end();
 
