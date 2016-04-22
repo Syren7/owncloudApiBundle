81d7751ee0473e8a55bc12fa260fb0bc53bc6715
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2016-04-22 21:57:24 +0200

    Fix: Dev: new caldav interface

--- Service/OwncloudCalendar.php
+++ Service/OwncloudCalendar.php
@@ -2,8 +2,8 @@
 
 namespace Syren7\OwncloudApiBundle\Service;
 
+use it\thecsea\simple_caldav_client\CalDAVCalendar;
 use Syren7\OwncloudApiBundle\lib\CalDavClient;
-use Syren7\OwncloudApiBundle\Model\Calendar;
 
 class OwncloudCalendar {
 
@@ -23,7 +23,7 @@ class OwncloudCalendar {
 		//Create settings array for curl connection
 		$settings = array(
 			//the webdav url ist created from your owncloud url + '/remote.php/webdav/' + your specific folder if wished
-			'baseUri' => $ocHost.'/remote.php/caldav/calendars/'.$ocUser.'/',
+			'baseUri' => $ocHost.'/remote.php/caldav/',
 			'userName' => $ocUser,
 			'password' => $ocPass,
 		);
@@ -31,12 +31,13 @@ class OwncloudCalendar {
 		$this->cal = new CalDavClient($settings);
 	}
 
-	public function getEvents(Calendar $calendar) {
+	public function getEvents(CalDAVCalendar $calendar) {
 		return $this->cal->getEvents($calendar);
 	}
 
-	public function createEvent(Calendar $cal, $text) {
-		return $this->cal->createEvent($cal, $text);
+	public function createEvent() {
+		//ToDo: This feature is currently missing
+		//return $this->cal->createEvent($cal, $text);
 	}
 
 	public function getCalendars() {
--- lib/CalDavClient.php
+++ lib/CalDavClient.php
@@ -3,147 +3,93 @@
 namespace Syren7\OwncloudApiBundle\lib;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Sabre\DAV\Client;
-use Sabre\DAV\Exception;
-use Sabre\DAV\Property;
-use Sabre\DAV\XMLUtil;
-use Sabre\HTTP\ClientException;
-use Sabre\HTTP\Request;
-use Sabre\HTTP\RequestInterface;
-use Sabre\HTTP\ResponseInterface;
+use it\thecsea\simple_caldav_client\CalDAVCalendar;
+use it\thecsea\simple_caldav_client\CalDAVException;
+use it\thecsea\simple_caldav_client\CalDAVObject;
 use Sabre\VObject;
 use Syren7\OwncloudApiBundle\Model\Calendar;
 use Syren7\OwncloudApiBundle\Model\CalendarEvent;
+use it\thecsea\simple_caldav_client\SimpleCalDAVClient;
 
 /**
- * This class extends the standard DAV client to an calDAV client
- *
- * See @url(http://sabre.io/dav/building-a-caldav-client/) for more information
- *
  * Class CalDavClient
  *
- * @extends Sabre\DAV\Client
  * @package Syren7\OwncloudApiBundle\lib
  */
-class CalDavClient extends Client{
-
-	const CALDAV_PROP_CALENDARETAG = '{DAV:}getetag';
-	const CALDAV_PROP_CALENDARNAME = '{DAV:}displayname';
-	const CALDAV_PROP_CALENDARDATA = '{urn:ietf:params:xml:ns:caldav}calendar-data';
-
+class CalDavClient {
 	/**
 	 * if there was an error you can retreive the last error with getLastException()
-	 * @var Exception $lastException
+	 * @var \Exception $lastException
 	 */
 	private $lastException = null;
 	/**
-	 * Save username for later use from settings
-	 * @var string $user
+	 * @var SimpleCalDAVClient $client
 	 */
-	private $user;
+	private $client;
 	/**
-	 * Save password for later use from settings
-	 * @var string $pass
-	 */
-	private $pass;
-
-	/**
-	 * ocCalendarConnector constructor.
+	 * CalDavClient constructor.
 	 *
-	 * @param array  $settings
+	 * @param array $settings
 	 */
 	public function __construct($settings=array()) {
-		parent::__construct($settings);
-		$this->user = isset($settings['userName'])?$settings['userName']:'';
-		$this->pass = isset($settings['password'])?$settings['password']:'';
+		//ToDo: check settings for required keys
+		//initiate CalDAVClient
+		$this->client = new SimpleCalDAVClient();
+		//connect to caldav server
+		$this->client->connect($settings['baseUri'], $settings['userName'], $settings['password']);
 	}
-
-	/**
-	 * Overwrite this class of http client because authentification and url settings didn't match
-	 *
-	 * @param RequestInterface $request
-	 *
-	 * @return ResponseInterface
-	 * @throws ClientException
-	 */
-	protected function doRequest(RequestInterface $request) {
-
-		$settings = $this->createCurlSettingsArray($request);
-		//don't know why but this fixes url error from HTTP/Client in SabreDav
-		$ch = curl_init($this->baseUri);
-
-		curl_setopt_array($ch, $settings);
-		//we have tu re-set the authentification parameters
-		curl_setopt($ch, CURLOPT_USERPWD, $this->user.':'.$this->pass);
-		//execute statement
-		$response = $this->curlExec($ch);
-
-		$response = $this->parseCurlResult($response, $ch);
-
-		if ($response['status'] === self::STATUS_CURLERROR) {
-			throw new ClientException($response['curl_errmsg'], $response['curl_errno']);
-		}
-
-		return $response['response'];
-
-	}
-
-
 	/**
 	 * Reads all calendars from a calDAV URL
 	 *
-	 * @return ArrayCollection with Calendar objects
+	 * @return ArrayCollection with CalDAVCalendar objects
 	 */
 	public function getCalendars() {
-		$calendars = new ArrayCollection();
 		try {
-			foreach($this->propFind($this->getAbsoluteUrl(), array(self::CALDAV_PROP_CALENDARNAME), 1) as $uri => $entry) {
-				//owncloud returns two calendards which are not named
-				//currently I don't know where they're from
-				//so we don't add them to the calendar collection
-				//ToDo: Find out where the empty calendards come from and possible reenable adding them to the collection
-				if(isset($entry[self::CALDAV_PROP_CALENDARNAME])) {
-					$calendars->add(new Calendar($uri, $entry[self::CALDAV_PROP_CALENDARNAME]));
-				}
-			}
+			$calendars = new ArrayCollection($this->client->findCalendars());
+			//return found calendars
+			return $calendars;
 		}
 		//if there was an error
-		catch(Exception $e) {
+		catch(\Exception $e) {
 			$this->setLastException($e);
+			//else on error return empty array collection
+			return new ArrayCollection();
 		}
-		//return found calendars
-		return $calendars;
 	}
 
 	/**
 	 * Returns an ArrayCollection object with VObject elements as childs if there are some entries
 	 *
-	 * @param Calendar  $calendar
+	 * @param CalDAVCalendar $calendar
 	 *
 	 * @return ArrayCollection
+	 * @throws CalDAVException
 	 */
-	public function getEvents(Calendar $calendar) {
+	public function getEvents(CalDAVCalendar $calendar) {
 		$entries = new ArrayCollection();
 		try {
-			foreach ($this->propReport($calendar->getUri(), array(
-				self::CALDAV_PROP_CALENDARETAG,
-				self::CALDAV_PROP_CALENDARDATA,
-			), 1) as $uri => $entry) {
+			//check calendar
+			if(!$calendar instanceof CalDAVCalendar) {
+				throw new CalDAVException("invalid calendar set", 1);
+			}
+			//set calendar
+			$this->client->setCalendar($calendar);
+			//go through every event and convert data
+			foreach ($this->client->getEvents() as $event) {
+				/**@var CalDAVObject $event*/
 				//Read VCalendar Object from calData
-				/**
-				 * @var VObject\Component\VCalendar $cal
-				 */
-				$cal = VObject\Reader::read($entry[self::CALDAV_PROP_CALENDARDATA]);
+				/** @var VObject\Component\VCalendar $cal */
+				$cal = VObject\Reader::read($event->getData());
 				//expand event if there are multiple events
 				$cal->expand((new \DateTime())->modify('-2 years'),(new \DateTime())->modify('+2 years'));
 				//go through all events
-				foreach($cal->VEVENT as $event) {
-					/**@var VObject\Component\VEvent $event*/
-					$entries->add(new CalendarEvent($event, $entry[self::CALDAV_PROP_CALENDARETAG]));
+				foreach($cal->VEVENT as $ev) {
+					/**@var VObject\Component\VEvent $ev*/
+					$entries->add(new CalendarEvent($ev, $event->getEtag()));
 				}
 			};
 		}
-		catch(Exception $e) {
+		catch(\Exception $e) {
 			$this->setLastException($e);
 		}
 
@@ -151,110 +97,14 @@ class CalDavClient extends Client{
 	}
 
 	public function createEvent(Calendar $calendar, $text) {
-		$url = $calendar->getUri().substr(bin2hex(random_bytes(32)), 0, 32).'.ics';
-
-		return $this->request('PUT',
-			  $url,
-			  $text,
-			  array(
-				  'Content-Type:' => 'text/calendar; charset=utf-8',
-			  )
-		);
-	}
-
-	/**
-	 * Executes a REPORT request to calDAV server
-	 *
-	 * @param       $url
-	 * @param array $properties
-	 * @param int   $depth
-	 *
-	 * @return array
-	 * @throws Exception
-	 * @throws \Sabre\HTTP\ClientException
-	 * @throws \Sabre\HTTP\ClientHttpException
-	 */
-	public function propReport($url, array $properties, $depth = 0) {
-		//create dom
-		$dom = new \DOMDocument('1.0', 'UTF-8');
-		$dom->formatOutput = true;
-		$root = $dom->createElementNS('urn:ietf:params:xml:ns:caldav', 'c:calendar-query');
-		$root->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:d', 'DAV:');
-		$prop = $dom->createElement('d:prop');
-		$filter = $dom->createElement('c:filter');
-		$dat = $dom->createElement('c:comp-filter');
-		$dat->setAttribute('name', 'VCALENDAR');
-		$filter->appendChild($dat);
-
-		foreach($properties as $property) {
-			list(
-				$namespace,
-				$elementName
-				) = XMLUtil::parseClarkNotation($property);
-
-			if ($namespace === 'DAV:') {
-				$element = $dom->createElement('d:'.$elementName);
-			} else {
-				$element = $dom->createElement('c:'.$elementName);
-			}
-
-			$prop->appendChild( $element );
-		}
-
-		$bla = $dom->appendChild($root);
-		$bla->appendChild( $prop );
-		$bla->appendChild( $filter );
-
-		$body = $dom->saveXML();
-		$url = $this->getAbsoluteUrl($url);
-
-		$request = new Request('REPORT', $url, [
-			'Depth' => $depth,
-			'Content-Type' => 'application/xml'
-		], $body);
-
-		$response = $this->send($request);
-
-		if ((int)$response->getStatus() >= 400) {
-			throw new Exception('HTTP error: ' . $response->getStatus());
-		}
-
-		$result = $this->parseMultiStatus($response->getBodyAsString());
-
-		// If depth was 0, we only return the top item
-		if ($depth===0) {
-			reset($result);
-			$result = current($result);
-			return isset($result[200])?$result[200]:[];
-		}
-
-		$newResult = [];
-		foreach($result as $href => $statusList) {
-
-			$newResult[$href] = isset($statusList[200])?$statusList[200]:[];
-
-		}
-
-		return $newResult;
-	}
-	###########################
-	######### Helpers #########
-	###########################
-	/**
-	 * Overwrite parent method so parameter $url is now optional
-	 * @param string $url
-	 *
-	 * @return string
-	 */
-	public function getAbsoluteUrl($url='') {
-		return parent::getAbsoluteUrl($url);
+		//ToDo: implement
 	}
 
 	###########################
 	### Getters and Setters ###
 	###########################
 	/**
-	 * @return Exception
+	 * @return \Exception
 	 */
 	public function getLastException() {
 		return $this->lastException;
@@ -267,7 +117,7 @@ class CalDavClient extends Client{
 		return $this->lastException !== null;
 	}
 	/**
-	 * @param Exception $e
+	 * @param \Exception $e
 	 *
 	 * @return CalDavClient
 	 */
