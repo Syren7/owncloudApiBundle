f7605ad49c7f92ae02ea2c2bc8578f185524203e
Konstantin Tümmler <konstantin.tuemmler@syren7.com>
2017-10-24 14:01:01 +0200

    New: URL check in settings to prevent the usage of double slashes in url

--- Service/OwncloudCalendar.php
+++ Service/OwncloudCalendar.php
@@ -27,9 +27,36 @@ class OwncloudCalendar {
 			'userName' => $ocUser,
 			'password' => $ocPass,
 		);
+
+		//New in  1.3.4
+		//fixes error, when accessing an URL with double slash (e.g. //) owncloud raises an error
+		//so we are going to search for double slashes and replace them with one slash
+		$settings['baseUri'] = $this->fixUrl($settings['baseUri']);
+
 		//create required connection objects
 		$this->cal = new CalDavClient($settings);
 	}
+
+
+	/**
+	 * Fixes double slash issue in urls
+	 * @param $checkUrl
+	 *
+	 * @return string
+	 */
+	private function fixUrl($checkUrl) {
+		//1. filter http / https
+		$proto = strpos($checkUrl, "https") !== false ? "https://" : "http://";
+		//1.1 remove leading https:// or http://
+		$url = str_replace($proto, "", $checkUrl);
+		//1.2 if the first sign is still an / than there were three slashes after http: or https:
+		if(substr($url, 0, 1) == "/") $url = substr($url, 1);
+		//2. replace all // to /
+		$url = preg_replace('/([^:])(\/{2,})/', "/", $url);
+		//3. create url
+		return $proto.$url;
+	}
+
 
 	public function getEvents(CalDAVCalendar $calendar) {
 		return $this->cal->getEvents($calendar);
